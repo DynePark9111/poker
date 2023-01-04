87672a47b701e6ca8b4efb1f66b9ec5a6682feb3
Dyne Park <dynepark9111@gmail.com>
2023-01-04 20:56:56 +0900

    react basic setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<h1>not found</h1>} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
