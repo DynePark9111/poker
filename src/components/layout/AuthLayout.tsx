930fb06946dda4e3a7de638958999d38a0fffa18
Dyne Park <dynepark9111@gmail.com>
2023-01-13 22:53:31 +0900

    feat: recaptcha v3

--- src/components/layout/AuthLayout.tsx
+++ src/components/layout/AuthLayout.tsx
@@ -1,10 +1,15 @@
 import styles from "../../styles/layout/AuthLayout.module.scss";
 import { Outlet } from "react-router-dom";
+import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
 
 export default function AuthLayout() {
+  const RECAPTCHA_CLIENT_KEY = import.meta.env.VITE_RECAPTCHA_CLIENT_KEY;
+
   return (
-    <div className={styles.AuthLayout}>
-      <Outlet />
-    </div>
+    <GoogleReCaptchaProvider reCaptchaKey={RECAPTCHA_CLIENT_KEY as string}>
+      <div className={styles.AuthLayout}>
+        <Outlet />
+      </div>
+    </GoogleReCaptchaProvider>
   );
 }
